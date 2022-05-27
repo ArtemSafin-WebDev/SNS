17482655b65dc937ef7e1f31eab5cd991efb8286
Artem Safin <artemsafin.webdev@gmail.com>
2022-05-27 04:32:53 +0300

    Документы

--- app.js
+++ app.js
@@ -36,18 +36,65 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("resize", setWidth);
 
-
   const setContainerWidth = () => {
-    const container = document.querySelector('.become-partner__inner-container');
+    const container = document.querySelector(
+      ".become-partner__inner-container"
+    );
     if (!container) return;
 
     document.documentElement.style.setProperty(
       "--partner-container-width",
       container.offsetWidth + "px"
     );
-  }
+  };
 
   setContainerWidth();
 
   window.addEventListener("resize", setContainerWidth);
+
+  function fileUploads() {
+    const elements = Array.from(document.querySelectorAll(".js-file-upload"));
+
+    elements.forEach((element) => {
+      const input = element.querySelector('input[type="file"]');
+      const label = element.querySelector(".js-file-upload-text");
+      const form = element.closest("form");
+
+      input.addEventListener("change", () => {
+        if (input.files.length) {
+          input.classList.add('loaded');
+          if (label) {
+            label.textContent = input.files[0].name;
+          }
+        
+        } else {
+          input.classList.remove('loaded');
+        }
+      });
+
+      input.addEventListener("dragenter", () => {
+        element.classList.add("dragged");
+      });
+      input.addEventListener("dragend", () => {
+        element.classList.remove("dragged");
+      });
+      input.addEventListener("dragleave", () => {
+        element.classList.remove("dragged");
+      });
+      input.addEventListener("drop", () => {
+        element.classList.remove("dragged");
+      });
+
+      if (form) {
+        form.addEventListener("reset", () => {
+          input.value = "";
+          label.innerHTML = originalLabelText;
+          element.classList.remove("file-loaded");
+          element.classList.remove("dragged");
+        });
+      }
+    });
+  }
+
+  fileUploads();
 });
